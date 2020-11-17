de1877e575f8df8a56b10a72704101c9e4d16f24
PhyoWin1993 <mr.phyo.1571993mm@gmail.com>
2020-11-17 23:42:46 +0630

    flutter update fb auth

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
 final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
